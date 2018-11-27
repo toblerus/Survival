92afadd1d2b16523dad6610bbe54ba62fc411c50
Tobi <toblerus@hotmail.de>
2018-11-27 01:00:07 +0100

    Added Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    void Update()
-    {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+    public int maxHealth;
+    public int currentHealth;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+    public Slider healthBar;
+    public Camera mainCamera;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    private void Awake()
+    {
+        Screen.lockCursor = true;
+        currentHealth = maxHealth;
+
+        healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+        mainCamera = Camera.main;
+
+    }
+        void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        transform.Translate(x, 0, z);
+        healthBar.value = currentHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
     }
 }
