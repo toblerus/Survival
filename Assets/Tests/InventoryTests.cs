bc827e4a8828e2152485a2e76c167a9bcda5c114
Daniel Bischoff <daniel.bischoff@fluffyfairygames.com>
2019-03-26 18:29:29 +0100

    fix usings

--- Assets/Tests/InventoryTests.cs
+++ Assets/Tests/InventoryTests.cs
@@ -1,30 +1,33 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Scripts.InventorySystem;
-using System;
 
-public class InventoryTests
+namespace Tests
 {
-    private const int DefaultSlotAmount = 12;
-
-    [Test]
-    public void Test_Initialization()
+    public class InventoryTests
     {
-        var inventory = new Inventory(DefaultSlotAmount);
-        inventory.contents = new ItemStack[DefaultSlotAmount];
-        Assert.AreEqual(12, inventory.Size);
-    }
+        private const int DefaultSlotAmount = 12;
 
-    [Test]
-    public void Test_SizeNull()
-    {
-        Assert.Throws<Exception>(() => new Inventory(0));
-    }
+        [Test]
+        public void Test_Initialization()
+        {
+            var inventory = new Inventory(DefaultSlotAmount);
+            inventory.contents = new ItemStack[DefaultSlotAmount];
+            Assert.AreEqual(12, inventory.Size);
+        }
 
-    [Test]
-    public void Test_AddItemStack()
-    {
-        var inventory = new Inventory(DefaultSlotAmount);
-        inventory.AddItemStack(3, 12);
-        Assert.AreEqual(12, inventory.contents[3].Amount);
+        [Test]
+        public void Test_SizeNull()
+        {
+            Assert.Throws<Exception>(() => new Inventory(0));
+        }
+
+        [Test]
+        public void Test_AddItemStack()
+        {
+            var inventory = new Inventory(DefaultSlotAmount);
+            inventory.AddItemStack(3, 12);
+            Assert.AreEqual(12, inventory.contents[3].Amount);
+        }
     }
 }
--- Assets/Tests/ItemStackTests.cs
+++ Assets/Tests/ItemStackTests.cs
@@ -1,77 +1,82 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
+using NUnit.Framework;
+using Scripts.InventorySystem;
 
-public class ItemStackTests {
-
-    private const int DefaultCapacity = 64;
-
-    [Test]
-    public void Test_Initialization() {
-        var itemStack = new ItemStack(DefaultCapacity);
-        Assert.IsTrue(itemStack.IsEmpty());
-        Assert.AreEqual(0, itemStack.Amount);
-        Assert.AreEqual(DefaultCapacity, itemStack.Capacity);
-    }
-
-    [Test]
-    public void Test_CustomCapacity()
+namespace Tests
+{
+    public class ItemStackTests
     {
-        var itemStack = new ItemStack(128);
-        Assert.AreEqual(128, itemStack.Capacity);
-    }
+        private const int DefaultCapacity = 64;
 
-    [Test]
-    public void Test_AddItems()
-    {
-        var itemStack = new ItemStack(DefaultCapacity);
-        itemStack.AddItems(5);
-        Assert.AreEqual(5, itemStack.Amount);
-    }
+        [Test]
+        public void Test_Initialization()
+        {
+            var itemStack = new ItemStack(DefaultCapacity);
+            Assert.IsTrue(itemStack.IsEmpty());
+            Assert.AreEqual(0, itemStack.Amount);
+            Assert.AreEqual(DefaultCapacity, itemStack.Capacity);
+        }
 
-    [Test]
-    public void Test_CapacityNull()
-    {
-        Assert.Throws<Exception>(() => new ItemStack(0));
-    }
+        [Test]
+        public void Test_CustomCapacity()
+        {
+            var itemStack = new ItemStack(128);
+            Assert.AreEqual(128, itemStack.Capacity);
+        }
 
-    [Test]
-    public void Test_DropItems()
-    {
-        var itemStack = new ItemStack(DefaultCapacity);
-        itemStack.AddItems(10);
-        itemStack.DropItems(5);
-        Assert.AreEqual(5, itemStack.Amount);
-    }
+        [Test]
+        public void Test_AddItems()
+        {
+            var itemStack = new ItemStack(DefaultCapacity);
+            itemStack.AddItems(5);
+            Assert.AreEqual(5, itemStack.Amount);
+        }
 
-    [Test]
-    public void Test_ExceedCapacity()
-    {
-        var addAmount = 100;
-        var itemStack = new ItemStack(DefaultCapacity);
-        Assert.Throws<Exception>(() => itemStack.AddItems(addAmount));
-        Assert.LessOrEqual(itemStack.Amount, DefaultCapacity);
-    }
+        [Test]
+        public void Test_CapacityNull()
+        {
+            Assert.Throws<Exception>(() => new ItemStack(0));
+        }
 
-    [Test]
-    public void Test_GetLeftoverSpace()
-    {
-        var itemStack = new ItemStack(DefaultCapacity);
-        itemStack.AddItems(10);
-        var Leftover = itemStack.GetLeftoverSpace();
-        Assert.AreEqual(54, Leftover);
-    }
+        [Test]
+        public void Test_DropItems()
+        {
+            var itemStack = new ItemStack(DefaultCapacity);
+            itemStack.AddItems(10);
+            itemStack.DropItems(5);
+            Assert.AreEqual(5, itemStack.Amount);
+        }
 
-    [Test]
-    public void Test_DropTooManyItems()
-    {
-        var itemStack = new ItemStack(DefaultCapacity);
-        itemStack.AddItems(10);
-        itemStack.DropItems(15);
-        Assert.GreaterOrEqual(itemStack.Amount, 0);
-    }
+        [Test]
+        public void Test_ExceedCapacity()
+        {
+            var addAmount = 100;
+            var itemStack = new ItemStack(DefaultCapacity);
+            Assert.Throws<Exception>(() => itemStack.AddItems(addAmount));
+            Assert.LessOrEqual(itemStack.Amount, DefaultCapacity);
+        }
 
-    private void InitializeItemStack(int bar)
-    {
-        new ItemStack(bar);
+        [Test]
+        public void Test_GetLeftoverSpace()
+        {
+            var itemStack = new ItemStack(DefaultCapacity);
+            itemStack.AddItems(10);
+            var Leftover = itemStack.GetLeftoverSpace();
+            Assert.AreEqual(54, Leftover);
+        }
+
+        [Test]
+        public void Test_DropTooManyItems()
+        {
+            var itemStack = new ItemStack(DefaultCapacity);
+            itemStack.AddItems(10);
+            itemStack.DropItems(15);
+            Assert.GreaterOrEqual(itemStack.Amount, 0);
+        }
+
+        private void InitializeItemStack(int bar)
+        {
+            new ItemStack(bar);
+        }
     }
 }
